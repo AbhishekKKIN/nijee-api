434ac6e46a3f39930a8a21a10dbb1b5667c53fb7
AbhishekKKIN <khatri169@gmail.com>
2023-02-12 15:17:16 +0530

    cors

--- app.js
+++ app.js
@@ -13,7 +13,19 @@ if (!config.get('PrivateKey')) {
 }
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: true, // "true" will copy the domain of the request back
+  // to the reply. If you need more control than this
+  // use a function.
+
+  credentials: true, // This MUST be "true" if your endpoint is
+  // authenticated via either a session cookie
+  // or Authorization header. Otherwise the
+  // browser will block the response.
+
+  methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+  // pre-flight OPTIONS requests
+}));
 app.use(express.urlencoded({ extended: true }));
 
 // Initialize DB
